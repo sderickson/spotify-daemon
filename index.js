c9d5b857c01c0f96f7cc92390b4b1efeb0b7ee81
Scott Erickson <sderickson@gmail.com>
2016-10-22 19:06:56 -0400

    Set up User model to store spotify user and auth information

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ var mongoose = require('mongoose');
 var session = require('express-session');
 var MongoStore = require('connect-mongo')(session);
 var _ = require('lodash');
-
+var moment = require('moment');
 
 app.use(express.static('./dist'))
 var isProduction = process.env.SPOTIFY_DAEMON_ENV === 'prod';
@@ -19,6 +19,20 @@ MONGO_DB = process.env.SPOTIFY_DAEMON_MONGO_DB || 'spotify-daemon'
 
 mongoUrl = `mongodb://${MONGO_HOST}:${MONGO_HOST}/${MONGO_DB}`
 mongoose.connect(mongoUrl);
+
+var UserSchema = new mongoose.Schema({
+  spotifyUser: {
+    display_name: String,
+    id: String
+  },
+  spotifyAuth: {
+    accessToken: String,
+    refreshToken: String,
+    scope: String,
+    expires: String
+  }
+})
+User = mongoose.model('User', UserSchema);
 
 app.use(session({
   secret: process.env.SPOTIFY_DAEMON_SESSION_SECRET || 'shh',
@@ -36,13 +50,12 @@ db.once('open', function() {
 // Don't use Users. Just store tokens and associate the sessions with them.
 
 
-
-REDIRECT_URI = 'http://localhost:3001/api/auth-spotify';
+REDIRECT_URL = process.env.SPOTIFY_DAEMON_REDIRECT_URL || 'http://localhost:3001/api/auth-spotify';
 CLIENT_ID = process.env.SPOTIFY_DAEMON_CLIENT_ID;
 CLIENT_SECRET = process.env.SPOTIFY_DAEMON_CLIENT_SECRET;
 
 app.get('/api/begin-spotify-oauth', wrap(function* (req, res) {
-  redirect_uri = encodeURIComponent(REDIRECT_URI);
+  redirect_uri = encodeURIComponent(REDIRECT_URL);
   response_type = 'code';
   scope = [
     'user-library-modify',
@@ -67,19 +80,45 @@ app.get('/api/auth-spotify', wrap(function* (req, res) {
   form = {
     grant_type: 'authorization_code',
     code,
-    redirect_uri: REDIRECT_URI
+    redirect_uri: REDIRECT_URL
   }
   tokenRes = yield request.postAsync({url, form, auth, json: true});
-  req.session.accessToken = tokenRes.body.access_token;
+
+  url = 'https://api.spotify.com/v1/me';
+  headers = {
+    'Authorization': `Bearer ${tokenRes.body.access_token}`
+  }
+  meRes = yield request.getAsync({url, headers, json: true});
+
+  user = yield User.findOne({'spotifyUser.id': meRes.body.id});
+  if(!user)
+    user = new User({spotifyUser: _.pick(meRes.body, 'id', 'display_name')});
+  user.set('spotifyAuth', {
+    accessToken: tokenRes.body.access_token,
+    refreshToken: tokenRes.body.refresh_token,
+    scope: tokenRes.body.scope,
+    expires: moment().add(tokenRes.body.expiresIn, 'seconds').toISOString(),
+    created: new Date().toISOString()
+  })
+  yield user.save()
+
+  req.session.userId = user.id;
   res.send(tokenRes.body)
 }));
+
+app.use('/api/*', wrap(function* (req, res, next) {
+  if(req.session.userId) {
+    req.user = yield User.findById(req.session.userId)
+  }
+  next()
+}))
 
 app.get('/api/alarm-tracks', wrap(function* (req, res) {
   playlist_id = '5hJleJv9tSNik6LKg0vaLB';
   user_id = 'sderickson';
   url = `https://api.spotify.com/v1/users/${user_id}/playlists/${playlist_id}/tracks`
   headers = {
-    'Authorization': `Bearer ${req.session.accessToken}`
+    'Authorization': `Bearer ${req.user.get('spotifyAuth.accessToken')}`
   }
   playlistResponse = yield request.getAsync({url, headers, json: true})
   res.json(playlistResponse.body)
@@ -90,7 +129,7 @@ app.get('/api/random-alarm', wrap(function* (req, res) {
   url = 'https://api.spotify.com/v1/me/albums'
   qs = { limit: 1 }
   headers = {
-    'Authorization': `Bearer ${req.session.accessToken}`
+    'Authorization': `Bearer ${req.user.get('spotifyAuth.accessToken')}`
   }
   numAlbumsResponse = yield request.getAsync({url, headers, qs, json: true})
   total = numAlbumsResponse.body.total
@@ -115,5 +154,6 @@ port = process.env.SPOTIFY_DAEMON_PORT || 3001;
 app.listen(port, function () {
   console.log(`Spotify Daemon listening on port ${port}!`);
 });
+
 
 
